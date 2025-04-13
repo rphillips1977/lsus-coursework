84daa2a6ce1a9a6b6b5c1b9c433b0e3ba4db95c4
rphillips1977 <167939712+rphillips1977@users.noreply.github.com>
2025-04-13 14:05:14 -0500

    Update Stored Procedure Hello World.sql

--- database-implementation/stored-procedures/Stored Procedure Hello World.sql
+++ database-implementation/stored-procedures/Stored Procedure Hello World.sql
@@ -1,11 +1,23 @@
-Alter Proc spHello
-As
-Begin
-Select 'Hello, World!'
-end
+-- =============================================
+-- Author:        [Robert Lovelace]
+-- Procedure:     spHello
+-- Description:   Simple stored procedure that returns a "Hello, World!" message.
+-- =============================================
 
-go
+-- If the procedure already exists, drop it
+IF OBJECT_ID('spHello', 'P') IS NOT NULL
+    DROP PROCEDURE spHello;
+GO
 
-spHello
+-- Create the stored procedure
+CREATE PROCEDURE spHello
+AS
+BEGIN
+    -- Return a simple text message
+    SELECT 'Hello, World!' AS Message;
+END
+GO
 
-go
+-- Execute the stored procedure
+EXEC spHello;
+GO
