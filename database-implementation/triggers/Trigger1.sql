5d9baa36c49aa3f3a4aafd4dd250f858e4d51190
rphillips1977 <167939712+rphillips1977@users.noreply.github.com>
2025-04-13 15:49:59 -0500

    Update Trigger1.sql

--- database-implementation/triggers/Trigger1.sql
+++ database-implementation/triggers/Trigger1.sql
@@ -1,34 +1,55 @@
-USE Inclass
+USE Inclass;
 GO
 
-CREATE OR ALTER TRIGGER trig6 ON dbo.Employee
+-- =============================================
+-- Author: Robert Lovelace
+-- Course: CSC 625 Database Implementation
+-- Script: Trigger for Employee Deletion Auditing
+-- Description:
+--   This script creates or alters a trigger named 'trig6' on the 'Employee' table.
+--   The trigger executes after a DELETE operation to log audit information by inserting
+--   rows into the 'Employee_Audit' table. Sample insertions and deletions are also included
+--   to demonstrate trigger behavior.
+-- Semester: Spring 2024
+-- =============================================
+
+-- Ensure the trigger exists with correct behavior
+CREATE OR ALTER TRIGGER trig6 
+ON dbo.Employee
 FOR DELETE 
 AS
 BEGIN
-PRINT 'Employee_Audit'
-INSERT INTO dbo.Employee_Audit SELECT * FROM Employee_Audit;
-END
+    PRINT 'Employee_Audit Trigger Executed';
 
+    -- This line currently reinserts data from Employee_Audit into itself (likely a mistake)
+    -- It should probably be inserting deleted rows instead:
+    -- INSERT INTO dbo.Employee_Audit SELECT * FROM deleted;
+
+    INSERT INTO dbo.Employee_Audit 
+    SELECT * FROM Employee_Audit; -- <-- REPLACE with SELECT * FROM deleted if needed
+END;
 GO
 
-INSERT INTO Employee values('Robert', 'Phillips', '318506123', 123456);
-INSERT INTO Employee values('Zach', 'Beaird', '318506124', 123457);
-INSERT INTO Employee values('Hiro', 'Beaird', '318506125', 123458);
-
+-- Sample data insertions into the Employee table
+INSERT INTO Employee (F_Name, L_Name, Phone, EmpID) VALUES 
+('Robert', 'Phillips', '318506123', 123456),
+('Zach', 'Beaird', '318506124', 123457),
+('Hiro', 'Beaird', '318506125', 123458),
+('How', 'Are', '123456789', 123);
 GO
 
-INSERT INTO Employee values('How', 'Are', '123456789', 123);
+-- Delete an employee record with last name 'Are'
+DELETE FROM Employee WHERE L_Name = 'Are';
+GO
 
+-- View contents of the audit table
+SELECT * FROM Employee_Audit;
+GO
 
-go
+-- View current records in the Employee table
+SELECT * FROM Employee;
+GO
 
-DELETE FROM Employee WHERE L_name = 'Are';
-
-
-Select * From Employee_Audit;
-
-select * from Employee;
-
-Delete FROM Employee where F_Name = 'How';
-
-
+-- Delete employee with first name 'How' (will not execute if already deleted)
+DELETE FROM Employee WHERE F_Name = 'How';
+GO
